1d42d6659012dfdf53f4bd5410eb3dd0eb4446e0
abishekvp <abishek.vp13@gmail.com>
2023-03-14 23:45:20 +0530

    14-3

---
+++ js/popup.js
@@ -0,0 +1,35 @@
+function showPopup(popupId) {
+  var popup = document.getElementById(popupId);
+  popup.style.display = "block";
+}
+
+function hidePopup(popupId) {
+  var popup = document.getElementById(popupId);
+  popup.style.display = "none";
+}
+
+    
+    
+    /*const popupButton = document.getElementById('popup-button');
+    const popupOverlay = document.getElementById('popup-overlay');
+    const closeButton = document.getElementById('close-button');
+    
+    popupButton.addEventListener('click', function() {
+        popupOverlay.style.display = 'block';
+    });
+    
+    closeButton.addEventListener('click', function() {
+        popupOverlay.style.display = 'none';
+    });
+
+    const popupButton1 = document.getElementById('popup-button1');
+    const popupOverlay1 = document.getElementById('popup-overlay1');
+    const closeButton1 = document.getElementById('close-button1');
+    
+    popupButton1.addEventListener('click', function() {
+        popupOverlay1.style.display = 'block';
+    });
+    
+    closeButton1.addEventListener('click', function() {
+        popupOverlay1.style.display = 'none';
+    });*/
